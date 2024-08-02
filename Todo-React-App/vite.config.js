02ae1872dd221df4b00b1b69cdc886ddcf28fe72
ThasleemPeer <thasleempeer361@gmail.com>
2024-08-02 19:10:18 +0530

    Deploy

--- Todo-React-App/vite.config.js
+++ Todo-React-App/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Todo-React-App/"
+  base:"/Todo-App-React/"
 })
